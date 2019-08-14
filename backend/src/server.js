2b523732481847146f5dd39547e269b7fb19cd7b
Édipo <edipo@brsis.com.br>
2019-08-14 20:45:06 -0300

    backend

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoose = require('cors');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://edipo:edipo@cluster0-aquzn.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+
+
+server.listen(3333);
